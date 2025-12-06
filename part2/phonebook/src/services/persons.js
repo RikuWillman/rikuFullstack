7f7ae030f8c1ed1326c678f66b012849324bb744
riku, willman <AB6906@student.jamk.fi>
2025-12-06 14:46:11 +0200

    Fixed the issue hopefully

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://rikufullstack-phonebook.onrender.com';
+const baseUrl = 'https://rikufullstack-phonebook.onrender.com/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl);
