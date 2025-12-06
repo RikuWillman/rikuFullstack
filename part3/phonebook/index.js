b138063e149ce08d2655772e5c89b997f12f0dc2
riku, willman <AB6906@student.jamk.fi>
2025-12-06 14:58:30 +0200

    Fixed a completely different issue

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -96,7 +96,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(express.static('dist'));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
